d5dc9c94dbad9e95e1982133af5678048633172d
Adam Ullman <adam@tokbox.com>
2016-12-09 15:26:35 +1100

    Remove chrome checks for deploy

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ function doCompile {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$BROWSER" != "chrome" -o "$BVER" != "stable" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH"]; then
     echo "Skipping deploy."
     exit 0
 fi
