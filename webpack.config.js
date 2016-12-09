898e82a52e9502a91fb4c696a64daabd4a1ade0a
Adam Ullman <adam@tokbox.com>
2016-12-10 07:29:07 +1100

    Don't actually need opentok at all

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const webpack = require('webpack');
-const OpenTok = require('opentok');
-const opentok = new OpenTok(process.env.OT_API_KEY, process.env.OT_API_SECRET);
 
 module.exports = {
   entry: {
@@ -17,15 +15,4 @@ module.exports = {
       { test: /\.svg$/, loader: 'url-loader' },
     ],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      config: {
-        OT_API_KEY: JSON.stringify(process.env.OT_API_KEY),
-        OT_SESSION_ID: JSON.stringify(process.env.OT_SESSION_ID),
-        OT_TOKEN: JSON.stringify(opentok.generateToken(process.env.OT_SESSION_ID, {
-          expireTime: (new Date().getTime() / 1000) + (2 * 24 * 60 * 60), // in 2 days
-        })),
-      },
-    }),
-  ],
 };
