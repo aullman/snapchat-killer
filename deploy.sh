d8c82d49cdbdaeb6a6dd444fa1517e0496d627c3
Adam Ullman <adam@tokbox.com>
2016-12-09 16:00:37 +1100

    Fix location of deploy key

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ TARGET_BRANCH="gh-pages"
 function doCompile {
   webpack
   cp -r js out/
-  cp -r index.html out/
+  cp index.html out/
   cp -r images out/
 }
 
@@ -34,7 +34,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf out/* || exit 0
 
 # Run our compile script
 doCompile
